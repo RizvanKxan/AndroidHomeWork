2a7f7c187e857d1ab3ac08e9b33f6e648cb1c4aa
Gromyko Leonid <Dark-seizer@yandex.ru>
2022-04-23 16:46:36 +0300

    Добавлена иконка для отоображения решённых преступлений и логика её отоображения.

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -49,6 +50,7 @@ public class CrimeListFragment extends Fragment {
 
         private TextView mTitleTextView;
         private TextView mDateTextView;
+        private ImageView mSolvedImageView;
         private Crime mCrime;
 
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
@@ -57,6 +59,7 @@ public class CrimeListFragment extends Fragment {
             itemView.setOnClickListener(this);
             mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+            mSolvedImageView = (ImageView) itemView.findViewById(R.id.crime_solved);
         }
 
         // Метод будет вызываться каждый раз, когда RecyclerView потребует связать
@@ -65,6 +68,7 @@ public class CrimeListFragment extends Fragment {
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
+            mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
         }
 
         @Override
